f713d486b5b233b2ccade309fe2f6a20d9a9100f
Travis CI User <travis@example.org>
2018-03-27 06:59:03 +0000

    Documentation upload

---
+++ implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix_web"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"actix_web/struct.Path.html\" title=\"struct actix_web::Path\">Path</a>&lt;T&gt;",synthetic:false,types:["actix_web::extractor::Path"]},{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"actix_web/struct.Query.html\" title=\"struct actix_web::Query\">Query</a>&lt;T&gt;",synthetic:false,types:["actix_web::extractor::Query"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"actix_web/fs/struct.NamedFile.html\" title=\"struct actix_web::fs::NamedFile\">NamedFile</a>",synthetic:false,types:["actix_web::fs::NamedFile"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
